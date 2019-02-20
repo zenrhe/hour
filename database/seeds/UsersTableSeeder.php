6072f91a6ae4e97673c73e28bacb5df26537f3c6
Rowan Evenstar <zenrhe@gmail.com>
2019-02-20 23:19:25 +0000

    trying to sort user seeder'

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+
+        //Faker ERROR---------
         //Create Single User
         //factory(App\User::class)->create();
 
         //Create Multiple Users
-        $count = 10;
-        factory(App\User::class,$count)->create();
+        //$count = 10;
+        //factory(App\User::class,$count)->create();
+
+        
+        User::create(array(
+            'username' => 'zenrhe@gmail.com',
+            'password' => Hash::make('test')
+        ));
+            
     }
 }
