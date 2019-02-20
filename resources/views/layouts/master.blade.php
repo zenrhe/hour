647aac922b9a180b71d29b63f262c90c8aef27fb
Rowan Evenstar <zenrhe@gmail.com>
2019-02-20 21:56:16 +0000

    css issue with main.css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('pageTitle')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="{{ HTML::style('css/main.css'); }}"/>
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="{{ HTML::style('css/main.css'); }}"/> -->
     
     <!-- <script src="main.js"></script> -->
 
