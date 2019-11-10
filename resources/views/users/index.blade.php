27012cc9222ece05a13238ae893441fe535aafdf
Rowan Evenstar <zenrhe@gmail.com>
2019-11-10 20:17:32 +0000

    Seperated first name avars as was incorrect syntax

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -18,7 +18,7 @@
         </thead>
             @foreach($users as $user)
             <tr>
-                <td><a href='/users/{{ $user->id }}'> {{ $user->first_name, $user->last_name }}</a></td>
+                <td><a href='/users/{{ $user->id }}'> {{ $user->first_name }} {{ $user->last_name }}</a></td>
                 <td>{{ $user->logs->where('submitted', '>=', Carbon\Carbon::now()->startOfMonth())->sum('hours') }}</td>
                 <td>{{ $user->logs->sum('hours') }}</td>
            </tr>
