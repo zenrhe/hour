50387719b83597d4e0ed2ef2609afad99b33e395
Rowan Evenstar <zenrhe@gmail.com>
2019-11-11 02:39:11 +0000

    Removed commented out code to diff file

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,144 +1,30 @@
 <div class="nav_wrapper">
-<a class="navbar-brand">Volunteer Hours</a>
-<div class="navbar">
-  <!-- <a href="/">Home</a> -->
-  @guest
-     <a href="{{ route('login') }}">Login</a> 
-     <a href="{{ route('register') }}">Register</a> 
-    @else
-    <!-- <p style="margin-right:1em;">{{ Auth::user()->first_name }}</p> -->
-    <a  href="/logs/create" class="btn btn-primary" role="button" >Add</a>
-    <a  href="/users/{{ Auth::user()->id }}" class="btn btn-primary" role="button" >View</a>
+  <a href="{{ url('/') }}"class="navbar-brand">Volunteer Hours</a>
+  <div class="navbar">
 
-<!-- <a href="#">Add</a> 
-     <a href="#">View</a>  -->
-     <a href="{{ route('logout') }}"
-        onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-        Logout
-    </a> 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        {{ csrf_field() }}
-    </form>
+    @guest
+      <a href="{{ route('login') }}">Login</a> 
+      <a href="{{ route('register') }}">Register</a> 
+      @else
+        <!-- Show Add and View Buttons -->
+        <a  href="/logs/create" class="btn btn-primary" role="button" >Add</a>
+        <a  href="/users/{{ Auth::user()->id }}" class="btn btn-primary" role="button" >View</a>
+
+        <!-- Link to User Profile -->
+        <a href="/profile/{{ Auth::user()->id }}">
+          <i class="fas fa-user"></i> {{ Auth::user()->first_name }}
+        </a>
+
+        <!-- Logout -->
+        <a href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">
+          <i class="fas fa-sign-out-alt"></i> Logout
+        </a> 
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          {{ csrf_field() }}
+        </form>
     @endguest
-</div>
-</div>
-
-<!-- <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Volunteer Hours
-                    </a>
-                    @guest
-                        @else
-                        ( {{ Auth::user()->first_name }} ) 
-                    @endguest
-                    
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-</div> -->
-<!-- <div class="navbar-wrapper">
-      <div class="container">
-
-        <nav class="navbar navbar-inverse navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                    <li><a href="">Page 1</a>
-                    </li>
-                    <li><a href="">Page 2</a>
-                    </li>
-                    <li><a href="">Page 3</a>
-                    </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-      </div>
-    </div>
-</div> -->
-    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
-</nav> -->
-    <!-- Scripts -->
-    <!-- <script src="{{ asset('js/app.js') }}"></script> -->
+</div>
+
